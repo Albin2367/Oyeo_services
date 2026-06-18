4e9b3ff27c452f15177c90826027ba4cd23fb669
Babela Boris <Albinbabela@gmail.com>
2026-06-18 20:53:51 +0200

    Engagement

--- oyeo_backend/urls.py
+++ oyeo_backend/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('page-services.html', TemplateView.as_view(template_name='page-services.html'), name='services'),
     path('page-tarif.html', TemplateView.as_view(template_name='page-tarif.html'), name='tarif'),
     path('Propos.html', TemplateView.as_view(template_name='Propos.html'), name='propos'),
+    path('Engagement.html', TemplateView.as_view(template_name='Engagement.html'), name='engagement'),
 ]
 
 if settings.DEBUG:
